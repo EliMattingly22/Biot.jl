7e0d66dbaa27d7e7b5ba3d5f8b4c4c2f71b32f42
Eli Mattingly <ematting@mit.edu>
2022-06-16 08:41:32 -0400

    Update MakeSolenoid.jl

--- src/MakeSolenoid.jl
+++ src/MakeSolenoid.jl
@@ -21,7 +21,7 @@ This function makes an elliptic solenoid and calculates the inductance.
     NLayers -  Number of test points per layer
     NPtsPath  - number of points that each wire loop will be discretized into,
     PlotOn deternimes if the coil will be plotted as it builds
-        
+
 """
 function eval_Solenoid_Induct(
     N, #Number of turns
@@ -108,5 +108,51 @@ function EvalField_Centered(
     return sum(FieldCentered)
 end
 
+"""
+This function takes in a vector for each of the following:
+    Number of turns (NVec) in each of the coupled solenoids
+    r₁/₂ are the first and second radii of the elliptic cross section
+    L is the length of each solenoid section
+    Zoffset is the center offset distance of each solenoid. 
+
+    The solenoids are built symmetrically so the Z offset is the center location
+    
+"""
+function makeConcentricSolenoids(NVec,r₁Vec,r₂Vec,LVec,ZOffsetVec;
+    DownSampleFac=1,PlotOn=false,NPtsPath=50,NLayers=15)
+
+    NCoils = length(NVec)
+    PP_List = vcat([[MakeEllipSolenoidPointPath(NVec[i],r₁Vec[i],r₂Vec[i],LVec[i],ZOffsetVec[i];NPtsPath=NPtsPath)] for i in 1:NCoils]...)
+    LMat = zeros(NCoils,NCoils)
+
+    if PlotOn
+        for j in 1:NCoils
+            pygui(true)
+            plot3D(PP_List[j][:,1],PP_List[j][:,2],PP_List[j][:,3])
+        end
+    end
+
+    for j in 1:NCoils
+        OtherCoilsIndex = filter(x-> x!==j,1:NCoils) #Make a vector of ind of other coils
+
+        for k in OtherCoilsIndex
+            
+            Mut, Self, SavedBSelfArr = Mutual_L_TwoLoops(PP_List[j], PP_List[k];DownSampleFac=DownSampleFac,MeasLayers=NLayers,MinThreshold=1e-10,IncludeWireInduct=false,SaveΦ₁=false)
+            LMat[j,j] = Self
+            LMat[j,k] = Mut
+            LMat[k,j] = Mut
+        end
+
+    end
+    
+
+    return LMat,PP_List
+end
 
 
+function MakeEllipSolenoidPointPath(N,r₁,r₂,FullLength,Zoffset;
+    NPtsPath=100)
+    dL = FullLength / (N-1)  
+    DriveCoilPointPath = vcat([MakeEllip(r₁, r₂; Center = [0, 0, L+Zoffset], NPts = NPtsPath) for L in (-FullLength/2:dL:FullLength/2)]...)  
+    return DriveCoilPointPath
+end
